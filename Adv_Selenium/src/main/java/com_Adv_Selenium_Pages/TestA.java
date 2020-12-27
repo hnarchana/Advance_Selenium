7f6f3b3c52ba5aa48022cdac8d626bb68dcd54ee
apple <apple@192.168.1.42>
2020-12-27 19:59:43 +0530

    TestA and TestB class

---
+++ Adv_Selenium/src/main/java/com_Adv_Selenium_Pages/TestA.java
@@ -0,0 +1,10 @@
+package com_Adv_Selenium_Pages;
+
+public class TestA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Adv_Selenium/src/main/java/com_Adv_Selenium_Pages/TestB.java
@@ -0,0 +1,11 @@
+package com_Adv_Selenium_Pages;
+
+public class TestB {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
